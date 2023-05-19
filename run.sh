ffbb331dccff281ce77b53d90fcd50be90aa183d
dwerkjem <derekrneilson@gmail.com>
2023-05-19 05:12:29 -0600

    added install

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # This script is used to run the program
-
 if [ $# -eq 0 ]; then
-    python3 ~/DEVELOPER/GPTanyQuiz_and_flashcards/menuQT.py
+
+    python3 "$(pwd)"/python/menuQT.py
 else
-    python3 ~/DEVELOPER/GPTanyQuiz_and_flashcards/anyQuizeCLI.py
+    python3 "$(pwd)"/python/anyQuizeCLI.py
 fi
